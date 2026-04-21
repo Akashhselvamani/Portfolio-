d3783311a5c7d3b380927e13f5b9a122d664c880
Akashhselvamani <akashselva11@gmail.com>
2026-04-21 22:00:35 +0530

    Forms

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -53,22 +53,30 @@
     fetch(action, {
       method: 'POST',
       body: formData,
-      headers: {'X-Requested-With': 'XMLHttpRequest'}
+      headers: {'Accept': 'application/json'}
     })
     .then(response => {
       if( response.ok ) {
-        return response.text();
+        return response.json();
       } else {
-        throw new Error(`${response.status} ${response.statusText} ${response.url}`); 
+        return response.json().then(data => {
+          if (Object.hasOwn(data, 'errors')) {
+            throw new Error(data["errors"].map(error => error["message"]).join(", "));
+          } else {
+            throw new Error(`${response.status} ${response.statusText} ${response.url}`);
+          }
+        }).catch(() => {
+          throw new Error(`${response.status} ${response.statusText} ${response.url}`);
+        });
       }
     })
     .then(data => {
       thisForm.querySelector('.loading').classList.remove('d-block');
-      if (data.trim() == 'OK') {
+      if (data && data.ok) {
         thisForm.querySelector('.sent-message').classList.add('d-block');
         thisForm.reset(); 
       } else {
-        throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action); 
+        throw new Error(data ? JSON.stringify(data) : 'Form submission failed and no error message returned from: ' + action); 
       }
     })
     .catch((error) => {
